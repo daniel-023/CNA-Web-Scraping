71c1546148b126bc37a06bd194d240407732ad93
Daniel Handoyo <daniel023@e.ntu.edu.sg>
2023-12-25 21:42:15 +0800

    Removed redundant files

--- Selenium Setup.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-import requests
-from bs4 import BeautifulSoup
-
-driver = webdriver.Chrome()
-driver.get('https://www.channelnewsasia.com/latest-news')
-webpage = driver.page_source
-soup = BeautifulSoup(webpage, 'html.parser')
-print(soup.prettify())
